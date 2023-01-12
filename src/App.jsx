f17d82bed39229c077c5a8f99f05175107ab41f7
Bárbara Sagredo <barpaz.sc@gmail.com>
2023-01-11 22:33:54 -0300

    drag and drop done and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { DragDropContext } from "@hello-pangea/dnd";
 import Header from "./components/Header";
 import ToDoComputed from "./components/ToDoComputed";
 import ToDoCreate from "./components/ToDoCreate";
@@ -6,6 +7,14 @@ import ToDoFilter from "./components/ToDoFilter";
 import ToDoList from "./components/ToDoList";
 
 const initialStateToDo = JSON.parse(localStorage.getItem("toDos")) || [];
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = [...list];
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 const App = () => {
   const [toDos, setToDos] = useState(initialStateToDo);
@@ -59,30 +68,47 @@ const App = () => {
 
   const changeFilter = (filter) => setFilter(filter);
 
+
+  const handleDragEnd = (result) => {
+    const { destination, source } = result;
+    if (!destination) return;
+    if (
+        source.index === destination.index &&
+        source.droppableId === destination.droppableId
+    )
+        return;
+
+    setToDos((prevTasks) =>
+        reorder(prevTasks, source.index, destination.index)
+    );
+};
+
   return (
     <div
       className="min-h-screen
     bg-gray-200 
     bg-[url('./assets/images/bg-mobile-light.jpg')] 
-    md:bg-[url('./assets/images/bg-desktop-light.jpg')] 
-    dark:bg-slate-900 
-    dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]
-    md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] 
     bg-contain 
     bg-no-repeat 
-    transition-all 
+    transition-all
     duration-1000 
+    dark:bg-slate-900 
+    dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] 
+    md:bg-[url('./assets/images/bg-desktop-light.jpg')] 
+    md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] 
     "
     >
       <Header />
 
       <main className="container mx-auto mt-8 px-4 md:max-w-lg">
         <ToDoCreate createTodo={createTodo} />
-        <ToDoList
-          toDos={filteredToDos()}
-          deleteTodo={deleteTodo}
-          updateToDo={updateToDo}
-        />
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <ToDoList
+            toDos={filteredToDos()}
+            deleteTodo={deleteTodo}
+            updateToDo={updateToDo}
+          />
+        </DragDropContext>
         <ToDoComputed
           computedItemsLeft={computedItemsLeft}
           clearCompleted={clearCompleted}
